fd2a53f1f8905d26bc04d34fc74db46975d8ff5b
20161104574 <a20161104574@admindeiMac-12.local>
2018-09-14 17:39:21 +0800

    shigeshupaixu
    Weer

--- a+b.playground/Contents.swift
+++ a+b.playground/Contents.swift
@@ -2,14 +2,10 @@
 
 import UIKit
 
-var array: Array<Int> = [3, 6, 1, 7, 2, 4, 9, 5,8,0]
-// 从大到小对数组排序
-func xx(num1: Int, num2: Int) -> Bool {
-    return num1 > num2
-}
-array.sort(by: xx)
-array
-array.sort(by: {(num1: Int, num2: Int) -> Bool in return num1 > num2 })
-array
 
+var array: Array<Int> = [3, 6, 1, 7, 2, 4, 9, 5, 8,0]
+array.sort(by: {(num1, num2) in
+    return num1 > num2
+})
+array
 print(array)
